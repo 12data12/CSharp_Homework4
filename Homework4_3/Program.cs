574223e04a75b2935b5624333fa744afe7388ca1
Daria Dmitricheva <110775372+12data12@users.noreply.github.com>
2022-09-08 09:58:41 +0300

    Task3

---
+++ Homework4_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int [] array = new int [8];
+void FillArray (int [] numbers)
+{
+    int length = numbers.Length;
+    int index = 0;
+    while(index<length)
+    {
+        numbers[index] = new Random().Next(1,35);
+        index++;
+    }
+}
+void PrintArray (int [] mass)
+{
+    int length = mass.Length;
+    int index = 0;
+    while(index<length)
+    {
+        Console.Write(mass[index]+" ");
+        index++;
+    }
+}
+FillArray(array);
+PrintArray(array);
