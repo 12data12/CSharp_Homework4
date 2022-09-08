b75bc9a5db37c239b6a3db1c7e9e5a7c79e886dc
Daria Dmitricheva <110775372+12data12@users.noreply.github.com>
2022-09-08 09:48:51 +0300

    Task2

---
+++ Homework4_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write ("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+while(number > 0)
+{
+    result = result + number%10;
+    number = number/10;
+}
+Console.WriteLine($"Result is {result}");
